568eca21de8af5cc28a33f9cbfdfa42541690668
Anderson Meneses <anderson.meneses@gmail.com>
2025-02-06 12:12:56 -0300

    Moved the validarCPF to utils.service

--- src/@fuse/services/utils/utils.service.ts
+++ src/@fuse/services/utils/utils.service.ts
@@ -51,4 +51,32 @@ export class FuseUtilsService {
 
         return name;
     }
+
+    validarCPF(cpf: string): boolean {
+        // Remove caracteres não numéricos
+        const cpfLimpo = cpf.replace(/\D/g, '');
+
+        // Verifica se o CPF tem 11 dígitos ou é inválido
+        if (cpfLimpo.length !== 11 || /^(\d)\1+$/.test(cpfLimpo)) {
+            return false;
+        }
+
+        // Função auxiliar para cálculo do dígito verificador
+        const calcularDigito = (base: string, pesoInicial: number): number => {
+            const soma = base
+                .split('')
+                .reduce((acc, num, index) => acc + parseInt(num) * (pesoInicial - index), 0);
+            const resto = soma % 11;
+            return resto < 2 ? 0 : 11 - resto;
+        };
+
+        // Calcula os dois dígitos verificadores
+        const base = cpfLimpo.substring(0, 9);
+        const digito1 = calcularDigito(base, 10);
+        const digito2 = calcularDigito(base + digito1, 11);
+
+        // Verifica se os dígitos calculados são iguais aos fornecidos
+        return cpfLimpo === base + digito1 + digito2;
+    }
+
 }
--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -19,6 +19,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
 import { FuseAlertComponent, FuseAlertType } from '@fuse/components/alert';
+import { FuseUtilsService } from '@fuse/services/utils';
 import { AuthService } from 'app/core/auth/auth.service';
 
 @Component({
@@ -57,7 +58,8 @@ export class AuthSignInComponent implements OnInit {
         private _activatedRoute: ActivatedRoute,
         private _authService: AuthService,
         private _formBuilder: UntypedFormBuilder,
-        private _router: Router
+        private _router: Router,
+        private _utilsService: FuseUtilsService
     ) { }
 
     // -----------------------------------------------------------------------------------------------------
@@ -100,41 +102,11 @@ export class AuthSignInComponent implements OnInit {
             const value = control.value;
 
 
-            const isValid = this.validarCPF(value);
+            const isValid = this._utilsService.validarCPF(value);
             return isValid ? null : { invalidCpf: true };
         };
     }
 
-    validarCPF(cpf: string): boolean {
-        // Remove caracteres não numéricos
-        const cpfLimpo = cpf.replace(/\D/g, '');
-
-        const validCPF = "19073856060" // '00000010191'
-
-        if(cpfLimpo=== validCPF) return true;
-
-        // Verifica se o CPF tem 11 dígitos ou é inválido
-        if (cpfLimpo.length !== 11 || /^(\d)\1+$/.test(cpfLimpo)) {
-            return false;
-        }
-
-        // Função auxiliar para cálculo do dígito verificador
-        const calcularDigito = (base: string, pesoInicial: number): number => {
-            const soma = base
-                .split('')
-                .reduce((acc, num, index) => acc + parseInt(num) * (pesoInicial - index), 0);
-            const resto = soma % 11;
-            return resto < 2 ? 0 : 11 - resto;
-        };
-
-        // Calcula os dois dígitos verificadores
-        const base = cpfLimpo.substring(0, 9);
-        const digito1 = calcularDigito(base, 10);
-        const digito2 = calcularDigito(base + digito1, 11);
-
-        // Verifica se os dígitos calculados são iguais aos fornecidos
-        return cpfLimpo === base + digito1 + digito2;
-    }
 
     // -----------------------------------------------------------------------------------------------------
     // @ Public methods
