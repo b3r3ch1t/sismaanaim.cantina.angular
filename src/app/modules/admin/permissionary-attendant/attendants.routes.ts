12a2874cac0a3173e269836e23bf609090fd6473
ASamee145 <sameeatheruk@gmail.com>
2025-03-24 08:15:25 +0500

    permissionary attendant

---
+++ src/app/modules/admin/permissionary-attendant/attendants.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PermissionaryAttendantComponent } from './attendants.component';
+
+export default [
+    {
+        path     : '',
+        component: PermissionaryAttendantComponent,
+    }
+] as Routes;
