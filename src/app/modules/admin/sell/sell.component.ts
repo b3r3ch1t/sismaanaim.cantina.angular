936af0d606120c8ea7ee0b6a8c9bb11cf349eb17
Anderson Meneses <anderson.meneses@gmail.com>
2025-04-02 19:47:59 -0300

    fix erro na venda

--- src/app/modules/admin/sell/sell.component.ts
+++ src/app/modules/admin/sell/sell.component.ts
@@ -22,247 +22,248 @@ import { CurrencyMaskDirective } from 'app/directives/currency-mask.directive';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 @Component({
-  selector: 'app-sell',
-  templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.css'],
-  imports: [
-    MatButtonModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatSelectModule,
-    CustomCurrencyPipe,
-    MatRadioModule,
-    MatSnackBarModule,
-    MatListModule,
-    CurrencyMaskDirective,
-    CurrencyMaskModule
-  ],
-  providers: [CurrencyPipe, CustomCurrencyPipe],
+    selector: 'app-sell',
+    templateUrl: './sell.component.html',
+    styleUrls: ['./sell.component.css'],
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        CustomCurrencyPipe,
+        MatRadioModule,
+        MatSnackBarModule,
+        MatListModule,
+        CurrencyMaskDirective,
+        CurrencyMaskModule
+    ],
+    providers: [CurrencyPipe, CustomCurrencyPipe],
 })
 
 export class AttendantSellComponent implements OnInit {
-  private readonly _httpClient = inject(HttpClient)
-  private readonly  _authService = inject(AuthService)
-  private readonly  _userService = inject(UserService);
+    private readonly _httpClient = inject(HttpClient)
+    private readonly _authService = inject(AuthService)
+    private readonly _userService = inject(UserService);
 
-  noClientFound = signal(false)
-  clients = signal([]);
-  paymentMethods = signal([]);
-  showClearButton = signal(false);
-  inputDisabled = signal(true);
-  selectedClient = signal(null);
-  selectedPaymentMethod = signal(null);
-  disableClientDropdown = signal(false);
-  disablePaymentMethodDropdown = signal(false);
-  validPaidInput = signal(null);
-  validRechargeInput = signal(null);
-  validPaymentMethod = signal(null);
-  currentAttendant = signal(null);
-  totalClientBalance = signal(0);
+    noClientFound = signal(false)
+    clients = signal([]);
+    paymentMethods = signal([]);
+    showClearButton = signal(false);
+    inputDisabled = signal(true);
+    selectedClient = signal(null);
+    selectedPaymentMethod = signal(null);
+    disableClientDropdown = signal(false);
+    disablePaymentMethodDropdown = signal(false);
+    validPaidInput = signal(null);
+    validRechargeInput = signal(null);
+    validPaymentMethod = signal(null);
+    currentAttendant = signal(null);
+    totalClientBalance = signal(0);
 
-  @ViewChild('cpfInput', { static: false }) cpfInput: ElementRef;
-  @ViewChild('clientDropdown', { static: false }) clientDropdown: MatRadioGroup;
-  @ViewChild('paymentMethodDropdown', { static: false }) paymentMethodDropdown: MatSelect;
-  @ViewChild('paidValueInput', { static: false }) paidValueInput: ElementRef;
-  @ViewChild('rechargeValueInput', { static: false }) rechargeValueInput: ElementRef;
+    @ViewChild('cpfInput', { static: false }) cpfInput: ElementRef;
+    @ViewChild('clientDropdown', { static: false }) clientDropdown: MatRadioGroup;
+    @ViewChild('paymentMethodDropdown', { static: false }) paymentMethodDropdown: MatSelect;
+    @ViewChild('paidValueInput', { static: false }) paidValueInput: ElementRef;
+    @ViewChild('rechargeValueInput', { static: false }) rechargeValueInput: ElementRef;
 
-  @ViewChild('sellingInput', { static: false }) sellingInput: ElementRef;
+    @ViewChild('sellingInput', { static: false }) sellingInput: ElementRef;
 
-  constructor(private snackbar: SnackbarService) { }
+    constructor(private snackbar: SnackbarService) { }
 
-  ngOnInit(): void {
-    this.loadAttendant()
-  }
-
-  handleSubmit() {
-    const unformatedValue = this.sellingInput.nativeElement.value
-    if (unformatedValue == "") {
-      this.snackbar.error("O valor de venda é obrigatório")
-      return
+    ngOnInit(): void {
+        this.loadAttendant()
     }
 
-    let sellingAmount = parseFloat(unformatedValue.replace('R$', '').replace('.', '').replace(',', '.'))
-    console.log("Selling Amount", sellingAmount, "total balance", this.totalClientBalance())
-    if (sellingAmount > this.totalClientBalance()) {
-      this.snackbar.error("O valor de venda deve ser menor que o saldo")
-      return
-    }
-
-    this._httpClient.post(`${environment.API_URL}atendente/realizarvenda`,
-      {
-        eventoId: this.currentAttendant().eventoId,
-        valor: sellingAmount,
-        clienteId: this.selectedClient().id,
-        permissionarioId: this.currentAttendant().permissionarioId
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${this._authService.accessToken}`
-        },
-      }).pipe(catchError(error => {
-        console.log(error)
-        throw error
-      })).subscribe((response: ApiResponse<any>) => {
-
-        if(response.success){
-
-            this.snackbar.success("Venda Realizada com sucesso!", 1000 * 10);
-            this.clearInputs();
-
-            return;
-
+    handleSubmit() {
+        const unformatedValue = this.sellingInput.nativeElement.value
+        if (unformatedValue == "") {
+            this.snackbar.error("O valor de venda é obrigatório")
+            return
         }
 
-        this.snackbar.error(response.message, 1000 * 10);
+        let sellingAmount = parseFloat(unformatedValue.replace('R$', '').replace('.', '').replace(',', '.'))
+        console.log("Selling Amount", sellingAmount, "total balance", this.totalClientBalance())
+        if (sellingAmount > this.totalClientBalance()) {
+            this.snackbar.error("O valor de venda deve ser menor que o saldo")
+            return
+        }
 
-      })
+        this._httpClient.post(`${environment.API_URL}atendente/realizarvenda`,
+            {
+                eventoId: this.currentAttendant().eventoId,
+                valor: sellingAmount,
+                clienteId: this.selectedClient().id,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${this._authService.accessToken}`
+                },
+            }).pipe(catchError(error => {
+                console.log(error)
+                throw error
+            })).subscribe((response: ApiResponse<any>) => {
 
-  }
+                if (response.success) {
 
-  loadAttendant() {
-    this._httpClient.get(`${environment.API_URL}atendente/getatendentebyid/${this._userService.user.id}`, {
-      headers: {
-        Authorization: `Bearer ${this._authService.accessToken}`
-      }
-    }).pipe(
-      catchError((error) => {
-        console.log(error);
-        throw error
-      })
-    ).subscribe((response: ApiResponse<any>) => {
-      if (response.success) {
-        this.currentAttendant.set(response.result)
+                    this.snackbar.success("Venda Realizada com sucesso!", 1000 * 10);
+                    this.clearInputs();
+
+                    return;
+
+                }
+
+                this.snackbar.error(response.message, 1000 * 10);
+
+            })
+
+    }
+
+    loadAttendant() {
+        this._httpClient.get(`${environment.API_URL}atendente/getatendentebyid/${this._userService.user.id}`, {
+            headers: {
+                Authorization: `Bearer ${this._authService.accessToken}`
+            }
+        }).pipe(
+            catchError((error) => {
+                console.log(error);
+                throw error
+            })
+        ).subscribe((response: ApiResponse<any>) => {
+            if (response.success) {
+                this.currentAttendant.set(response.result)
+                this.inputDisabled.set(false)
+            }
+
+        });
+    }
+
+    handleRechargeValueInput(event: KeyboardEvent) {
+        this.onKeyPress(event)
+    }
+
+    handlePaidValueInput(event: KeyboardEvent) {
+        this.onKeyPress(event)
+    }
+
+    onKeyPress(event: KeyboardEvent) {
+        console.log(event.key)
+
+        if (event.key === 'Enter' || event.key === '-' || event.key === ',') {
+            return
+        }
+
+        const allowedChars = /[0-9\.]/; // Allow numbers and a single decimal point
+        const inputChar = String.fromCharCode(event.keyCode || event.which);
+
+        if (!allowedChars.test(inputChar)) {
+            event.preventDefault();
+        }
+    }
+
+    handleCpfInput(event: InputEvent) {
+
+        this.noClientFound.set(false);
+        this.totalClientBalance.set(0);
+
+        const input = event.target as HTMLInputElement;
+        if (input.value.length >= 4) {
+            this.selectedClient.set(null)
+            this.clients.set([])
+            this.inputDisabled.set(true);
+
+            this._httpClient.get(`${environment.API_URL}clientes/getclientesbycpf/${input.value}`, {
+                headers: {
+                    "Authorization": `Bearer ${this._authService.accessToken}`
+                }
+            })
+                .pipe(catchError((error) => {
+                    console.log(error);
+                    throw error;
+                }))
+                .subscribe((data: ApiResponse<Array<{ id: string, nome: string }>>) => {
+                    if (data.success) {
+                        const clients = data.result
+                        this.clients.set(clients)
+
+                        if (this.clients().length === 1) {
+                            this.selectedClient.set(this.clients()[0]);
+                            this.handleClientSelection(this.selectedClient().id);
+
+                            this.totalClientBalance.set(0);
+                        }
+
+                        if (!this.clients().length) {
+                            this.noClientFound.set(false)
+                        }
+
+                    }
+                    else {
+                        this.snackbar.error("Nenhum cliente encontrado");
+                    }
+                    this.showClearButton.set(true)
+                });
+
+        } else {
+            this.showClearButton.set(false)
+        }
+    }
+
+    clearInputs() {
+        this.showClearButton.set(false)
         this.inputDisabled.set(false)
-      }
-
-    });
-  }
-
-  handleRechargeValueInput(event: KeyboardEvent) {
-    this.onKeyPress(event)
-  }
-
-  handlePaidValueInput(event: KeyboardEvent) {
-    this.onKeyPress(event)
-  }
-
-  onKeyPress(event: KeyboardEvent) {
-    console.log(event.key)
-
-    if (event.key === 'Enter' || event.key === '-' || event.key === ',') {
-      return
+        this.clients.set([])
+        this.selectedClient.set(null)
     }
 
-    const allowedChars = /[0-9\.]/; // Allow numbers and a single decimal point
-    const inputChar = String.fromCharCode(event.keyCode || event.which);
 
-    if (!allowedChars.test(inputChar)) {
-      event.preventDefault();
-    }
-  }
+    handleClientSelection(clientId: string) {
+        this.noClientFound.set(false)
+        if (clientId) {
+            this.disableClientDropdown.set(true)
+            this._httpClient.get(`${environment.API_URL}clientes/getclientebyid/${clientId}`, {
+                headers: {
+                    "Authorization": `Bearer ${this._authService.accessToken}`
+                }
+            })
+                .pipe(catchError((error) => {
+                    console.log(error);
+                    throw error;
+                }))
+                // TODO : Create a datatype for Client
+                // TODO : Move this to an API service
+                .subscribe((data: ApiResponse<object>) => {
+                    console.log(data)
+                    this.disableClientDropdown.set(false)
+                    if (data.success) {
+                        this.totalClientBalance.set(0);
+                        this.selectedClient.set(data.result);
+                        this.loadClientBalance();
+                    }
+                });
 
-  handleCpfInput(event: InputEvent) {
-
-    this.noClientFound.set(false);
-    this.totalClientBalance.set(0);
-
-    const input = event.target as HTMLInputElement;
-    if (input.value.length >= 4) {
-      this.selectedClient.set(null)
-      this.clients.set([])
-      this.inputDisabled.set(true);
-
-      this._httpClient.get(`${environment.API_URL}clientes/getclientesbycpf/${input.value}`, {
-        headers: {
-          "Authorization": `Bearer ${this._authService.accessToken}`
         }
-      })
-        .pipe(catchError((error) => {
-          console.log(error);
-          throw error;
-        }))
-        .subscribe((data: ApiResponse<Array<{ id: string, nome: string }>>) => {
-          if (data.success) {
-            const clients = data.result
-            this.clients.set(clients)
+    }
 
-            if(this.clients().length === 1){
-              this.selectedClient.set(this.clients()[0])
-              this.handleClientSelection(this.selectedClient().id)
+    loadClientBalance() {
+        this._httpClient.get(`${environment.API_URL}clientes/getsaldoclientebyclientid/${this.selectedClient().id}`, {
+            headers: {
+                "Authorization": `Bearer ${this._authService.accessToken}`
             }
-
-            if(!this.clients().length) {
-              this.noClientFound.set(false)
-            }
-
-          }
-          else{
-            this.snackbar.error("Nenhum cliente encontrado");
-          }
-          this.showClearButton.set(true)
-        });
-
-    } else {
-      this.showClearButton.set(false)
+        })
+            .pipe(catchError((error) => {
+                console.log(error);
+                throw error;
+            }))
+            .subscribe((data: ApiResponse<Array<{ id: string, saldo: number }>>) => {
+                if (data.success) {
+                    data.result.forEach(item => {
+                        this.totalClientBalance.update(old => old + item.saldo)
+                    })
+                }
+            });
     }
-  }
-
-  clearInputs() {
-    this.showClearButton.set(false)
-    this.inputDisabled.set(false)
-    this.clients.set([])
-    this.selectedClient.set(null)
-  }
-
-
-  handleClientSelection(clientId: string) {
-    this.noClientFound.set(false)
-    if (clientId) {
-      this.disableClientDropdown.set(true)
-      this._httpClient.get(`${environment.API_URL}clientes/getclientebyid/${clientId}`, {
-        headers: {
-          "Authorization": `Bearer ${this._authService.accessToken}`
-        }
-      })
-        .pipe(catchError((error) => {
-          console.log(error);
-          throw error;
-        }))
-        // TODO : Create a datatype for Client
-        // TODO : Move this to an API service
-        .subscribe((data: ApiResponse<object>) => {
-          console.log(data)
-          this.disableClientDropdown.set(false)
-          if (data.success) {
-            this.selectedClient.set(data.result)
-            console.log(this.selectedClient())
-            this.loadClientBalance()
-          }
-        });
-
-    }
-  }
-
-  loadClientBalance() {
-    this._httpClient.get(`${environment.API_URL}clientes/getsaldoclientebyclientid/${this.selectedClient().id}`, {
-      headers: {
-        "Authorization": `Bearer ${this._authService.accessToken}`
-      }
-    })
-      .pipe(catchError((error) => {
-        console.log(error);
-        throw error;
-      }))
-      .subscribe((data: ApiResponse<Array<{ id: string, saldo: number }>>) => {
-        if (data.success) {
-          data.result.forEach(item => {
-            this.totalClientBalance.update(old => old + item.saldo)
-          })
-        }
-      });
-  }
 
 }
