e8bac08e7aa6ac0e502b932a2487054dca190ba1
Anderson Meneses <anderson.meneses@gmail.com>
2025-04-05 05:08:56 -0300

    .

--- src/app/environments/environment.main.ts
+++ src/app/environments/environment.main.ts
@@ -2,27 +2,10 @@
 
 export enum BackendURL {
     Local = 'http://localhost:60397/v1/sismaanaimcia/',
-    Staging = 'https://apicantina.berechit.com.br/v1/sismaanaim/'
+    Staging = 'api/v1/sismaanaim/'
   }
 
 
-  export enum KEYCLOAKURL {
-    Staging= 'https://keycloak.berechit.com.br',
-    Local = 'https://localhost/'
-  }
-
-
-  export enum KEYCLOAKREALM {
-    Local = 'SisMaanaim',
-    Prod = 'SisMaanaim',
-    Staging = 'SisMaanaim'
-  }
-
-  export enum KEYCLOAKCLIENTID {
-    Local = 'SisMaanaim',
-    Prod = 'SisMaanaim',
-    Staging = 'SisMaanaim'
-  }
 
 
   export enum Checkout_URL {
@@ -33,16 +16,5 @@ export enum BackendURL {
   export const environment = {
     API_URL:BackendURL.Staging,
     production: false,
-    KEYCLOAK_URL:KEYCLOAKURL.Staging,
-    KEYCLOAK_REALM: KEYCLOAKREALM.Local,
-    KEYCLOAK_CLIENTID: KEYCLOAKCLIENTID.Local,
     Checkout_URL: Checkout_URL.Prod
   };
-
-  // const keycloakConfig = {
-  //   url: , // Keycloak server URL
-  //   realm: '<your-realm>',               // Your Keycloak realm
-  //   clientId: '<your-client-id>',        // Your Keycloak client ID
-  // };
-
-  // export const keycloak = new Keycloak(keycloakConfig);
