6ee6ef6b358db7a6d9966f7e95c6b43a780b8896
Anderson Meneses <anderson.meneses@gmail.com>
2025-04-18 14:49:28 -0300

    .

--- src/app/modules/admin/attendant-my-actions/attendant-my-actions.component.ts
+++ src/app/modules/admin/attendant-my-actions/attendant-my-actions.component.ts
@@ -11,98 +11,122 @@ import { MatSortModule, MatSort } from '@angular/material/sort';
 import { CustomCurrencyPipe } from 'app/pipes/custom-currency.pipe';
 import { CurrencyPipe, formatDate } from '@angular/common';
 import { CustomDatePipe } from 'app/pipes/custom-date.pipe';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-attendant-my-actions',
-  templateUrl: './attendant-my-actions.component.html',
-  styleUrls: ['./attendant-my-actions.component.css'],
-  imports: [
-    MatTableModule,
-    MatSortModule,
-    MatSort,
-    MatPaginator,
-    CustomCurrencyPipe,
-    CustomDatePipe
-  ],
-  providers: [
-    CurrencyPipe,
-  ],
+    selector: 'app-attendant-my-actions',
+    templateUrl: './attendant-my-actions.component.html',
+    styleUrls: ['./attendant-my-actions.component.css'],
+    imports: [
+        MatTableModule,
+        MatSortModule,
+        MatSort,
+        MatPaginator,
+        CustomCurrencyPipe,
+        CustomDatePipe,
+        MatFormFieldModule,
+        MatInputModule,
+    ],
+    providers: [
+        CurrencyPipe,
+    ],
 })
 export class AttendantMyActionsComponent implements OnInit, AfterViewInit {
 
-  private _httpClient: HttpClient = inject(HttpClient);
-  private _authService: AuthService = inject(AuthService);
-  private _userService: UserService = inject(UserService);
+    private readonly _httpClient: HttpClient = inject(HttpClient);
+    private readonly _authService: AuthService = inject(AuthService);
+    private readonly _userService: UserService = inject(UserService);
 
-  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+    @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+    @ViewChild(MatSort, { static: false }) sort!: MatSort;
 
-  history = signal([]);
-  dataSource = signal(new MatTableDataSource(this.history()));
+    history = signal([]);
+    dataSource = signal(new MatTableDataSource(this.history()));
 
-  displayedColumns = [
-    "clienteNome",
-    "data",
-    "valor",
-  ]
+    displayedColumns = [
+        "clienteNome",
+        "data",
+        "valor",
+        "transactionId",
+    ]
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-    this._httpClient.get(`${environment.API_URL}atendente/getatendentebyid/${this._userService.user.id}`, {
-      headers: {
-        Authorization: `Bearer ${this._authService.accessToken}`
-      }
-    }).pipe(
-      catchError((error) => {
-        console.log(error);
-        throw error
-      })
-    ).subscribe((response: ApiResponse<any>) => {
-      if (response.success) {
-
-        const eventId = response.result.eventoId.trim();
-
-        this._httpClient.get(`${environment.API_URL}atendente/getmyhistorybyeventoId/${eventId}`,{
-          headers: {
-            Authorization: `Bearer ${this._authService.accessToken}`
-          }
+    ngOnInit(): void {
+        this._httpClient.get(`${environment.API_URL}atendente/getatendentebyid/${this._userService.user.id}`, {
+            headers: {
+                Authorization: `Bearer ${this._authService.accessToken}`
+            }
         }).pipe(
-          catchError((error) => {
-            console.log(error);
-            throw error
-          })
+            catchError((error) => {
+                console.log(error);
+                throw error
+            })
         ).subscribe((response: ApiResponse<any>) => {
-          console.log(response.result)
-          this.history.set(response.result)
-          console.log(this.history())
-          const dataSource = new MatTableDataSource(this.history())
-          if(dataSource){
-            console.log("data source", dataSource)
+            if (response.success) {
 
-            console.log("paginator => ", this.paginator)
-            console.log("sort => ", this.sort)
+                const eventId = response.result.eventoId.trim();
 
-            dataSource.paginator = this.paginator;
-            dataSource.sort = this.sort;
-            this.dataSource.set(dataSource);
-          }
-        })
-
-
-
-      }
-
-    });
-  }
+                this._httpClient.get(`${environment.API_URL}atendente/getmyhistorybyeventoId/${eventId}`, {
+                    headers: {
+                        Authorization: `Bearer ${this._authService.accessToken}`
+                    }
+                }).pipe(
+                    catchError((error) => {
+                        console.log(error);
+                        throw error
+                    })
+                ).subscribe((response: ApiResponse<any>) => {
+                    this.history.set(response.result);
+                    const dataSource = new MatTableDataSource(this.history());
+                    if (dataSource) {
 
 
-  ngAfterViewInit(): void {
-    if (this.dataSource) {
-      this.dataSource().paginator = this.paginator;
-      this.dataSource().sort = this.sort;
+                        dataSource.paginator = this.paginator;
+                        dataSource.sort = this.sort;
+                        this.dataSource.set(dataSource);
+
+
+                    }
+                })
+
+
+
+            }
+
+        });
     }
-  }
 
+
+    ngAfterViewInit(): void {
+        if (this.dataSource) {
+            this.dataSource().paginator = this.paginator;
+            this.dataSource().sort = this.sort;
+        }
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+
+
+        const dataSource = new MatTableDataSource(this.history())
+        if (dataSource) {
+
+
+
+
+
+        }
+        dataSource.filter = filterValue.trim().toLowerCase();
+
+
+        dataSource.paginator = this.paginator;
+        dataSource.sort = this.sort;
+        this.dataSource.set(dataSource);
+
+
+
+    }
 
 }
