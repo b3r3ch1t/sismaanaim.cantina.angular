4e931bc01ae34989c74daa8d6f41431299559285
Anderson Meneses <anderson.meneses@gmail.com>
2025-06-02 11:52:35 -0300

    .

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -33,7 +33,7 @@ export enum Checkout_URL {
 }
 
 export const environment = {
-  API_URL:BackendURL.Staging,
+  API_URL:BackendURL.Local,
   production: false,
   KEYCLOAK_URL:KEYCLOAKURL.Staging,
   KEYCLOAK_REALM: KEYCLOAKREALM.Local,
